38e735a70b9d3b71c32273d691d1ac46feb848c4
C KEERTHI <71280538+KeeKs36@users.noreply.github.com>
2022-03-31 12:27:40 +0530

    Create existreg.c

---
+++ 3_Implementation/src/existreg.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+#include "regist.h"
+
+// Patients already registered list is given as input for verification
+// v stands for patient details
+int existregist(){
+    v[1].age=123;
+    v[1].aadhar=123;
+    v[1].phone_number=123;
+    v[1].secret_code=123;
+    v[1].vaccine_type=1;
+    v[1].vaccine_doses=1;
+    return 0;
+}
